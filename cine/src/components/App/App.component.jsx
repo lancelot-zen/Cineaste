bbb8a18387fba0827b89a5c176708d04516d5af6
scarvage <mojha001@gmail.com>
2022-11-04 21:12:21 +0530

    UI

--- cine/src/components/App/App.component.jsx
+++ cine/src/components/App/App.component.jsx
@@ -4,11 +4,11 @@ import Header from '../elements/Header/Header.component';
 import Home from '../Home/Home.component';
 import Movie from '../Movie/Movie.component';
 import NotFound from '../elements/NotFound/NotFound.component';
-
+import Sidebar from '../elements/headerTemp/sidebar';
 const App = () => (
   <BrowserRouter>
     <React.Fragment>
-      <Header />
+      <Sidebar />
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/:movieId" component={Movie} exact />
